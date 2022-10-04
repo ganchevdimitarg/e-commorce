5b03bb7b196c51062dfa3e7e12a28cca740cac96
DimitarGanchev <ganchevdimitarg@gmail.com>
2022-10-04 21:44:58 +0300

    initial commit

--- src/main/java/com/concordeu/ecommerce/ECommerceApplication.java
+++ src/main/java/com/concordeu/ecommerce/ECommerceApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ECommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
+
 	}
 }
