dcff57a568a369010e5d102cc2bf28a2455dc856
DimitarGanchev <ganchevdimitarg@gmail.com>
2022-11-22 19:36:48 +0200

    added swagger with gateway

--- profile/src/main/java/com/concordeu/profile/ProfileApplication.java
+++ profile/src/main/java/com/concordeu/profile/ProfileApplication.java
@@ -1,11 +1,16 @@
 package com.concordeu.profile;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@OpenAPIDefinition(info =
+@Info(title = "Profile API", description = "Documentation Profile API v1.0")
+)
 public class ProfileApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfileApplication.class, args);
