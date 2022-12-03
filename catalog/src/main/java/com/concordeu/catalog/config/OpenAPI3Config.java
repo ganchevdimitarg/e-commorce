639f662a99a618282a98bdbc0a166772a6b21514
DimitarGanchev <ganchevdimitarg@gmail.com>
2022-12-03 12:43:09 +0200

    refactor

--- catalog/src/main/java/com/concordeu/catalog/config/OpenAPI3Config.java
+++ catalog/src/main/java/com/concordeu/catalog/config/OpenAPI3Config.java
@@ -4,12 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.OAuthFlow;
-import io.swagger.v3.oas.annotations.security.OAuthFlows;
-import io.swagger.v3.oas.annotations.security.OAuthScope;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.*;
 import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         servers = {@Server(url = "https://localhost:8081")},
@@ -17,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
                 title = "Catalog Service APIs",
                 description = "This lists all the Catalog Service API Calls. The Calls are OAuth2 secured, so please use your client ID and Secret to test them out.",
                 version = "v1.0"))
+@SecurityRequirement(name = "security_auth")
 @SecurityScheme(
         name = "security_auth",
         type = SecuritySchemeType.OAUTH2,
@@ -29,5 +26,4 @@ import org.springframework.context.annotation.Configuration;
                 )
         )
 )
-@Configuration
 public class OpenAPI3Config {}
