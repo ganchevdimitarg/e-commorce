0ea66ac798e3f9178a43e977a2b1f2952fbbcbd5
DimitarGanchev <ganchevdimitarg@gmail.com>
2022-12-04 13:59:06 +0200

    refactor

---
+++ gateway/src/main/java/com/concordeu/gateway/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.concordeu.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        return http.csrf().disable().build();
+    }
+}
