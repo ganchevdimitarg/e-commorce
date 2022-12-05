366235cc72b0368afb6b9c547921a79d06be0658
DimitarGanchev <ganchevdimitarg@gmail.com>
2022-12-05 18:37:29 +0200

    refactor

--- catalog/src/main/java/com/concordeu/catalog/config/ResourceServerConfig.java
+++ catalog/src/main/java/com/concordeu/catalog/config/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.concordeu.catalog.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebSecurity
 public class ResourceServerConfig {
