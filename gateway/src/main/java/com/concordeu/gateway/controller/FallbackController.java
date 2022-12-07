ee6af516c2ca6e383fdad062cec19474152e5639
DimitarGanchev <ganchevdimitarg@gmail.com>
2022-12-07 16:01:05 +0200

    added circuitbreaker to gateway service

---
+++ gateway/src/main/java/com/concordeu/gateway/controller/FallbackController.java
@@ -0,0 +1,29 @@
+package com.concordeu.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/catalog-fallback")
+    Mono<String> getCategoryFallback() {
+        return Mono.just("Oops... Something went wrong, please try again later :)");
+    }
+
+    @GetMapping("/order-fallback")
+    Mono<String> getOrderFallback() {
+        return Mono.just("Oops... Something went wrong, please try again later :)");
+    }
+
+    @GetMapping("/profile-fallback")
+    Mono<String> getProfileFallback() {
+        return Mono.just("Oops... Something went wrong, please try again later :)");
+    }
+
+    @GetMapping("/notification-fallback")
+    Mono<String> getNotificationFallback() {
+        return Mono.just("Oops... Something went wrong, please try again later :)");
+    }
+}
