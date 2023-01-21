62aba2af2e5fcb83c0b6b260d5e6d50830ea8630
DimitarGanchev <ganchevdimitarg@gmail.com>
2023-01-21 18:09:21 +0200

    refactor

--- order/src/main/java/com/concordeu/order/config/OrderApplicationConfig.java
+++ order/src/main/java/com/concordeu/order/config/OrderApplicationConfig.java
@@ -1,26 +1,13 @@
 package com.concordeu.order.config;
 
 import com.google.gson.Gson;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
 
 @Configuration
 public class OrderApplicationConfig {
     @Bean
     public Gson gson() {
         return new Gson();
-    }
-
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> orderServiceCustomizer() {
-        return factory -> factory.configure(builder -> builder
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2)).build())
-                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()), "order-service");
     }
 }
--- profile/src/main/java/com/concordeu/profile/config/ProfileApplicationConfig.java
+++ profile/src/main/java/com/concordeu/profile/config/ProfileApplicationConfig.java
@@ -1,16 +1,10 @@
 package com.concordeu.profile.config;
 
 import com.google.gson.Gson;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.time.Duration;
 
 @Configuration
 public class ProfileApplicationConfig {
@@ -22,12 +16,5 @@ public class ProfileApplicationConfig {
     @Bean
     public Gson gson() {
         return new Gson();
-    }
-
-    @Bean
-    public Customizer<ReactiveResilience4JCircuitBreakerFactory> profileServiceCustomizer() {
-        return factory -> factory.configure(builder -> builder
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2)).build())
-                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()), "profile-service");
     }
 }
