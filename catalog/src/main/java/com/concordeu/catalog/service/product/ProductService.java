7765a57f24fcd37f176aa9bc9094c09c545de2ec
DimitarGanchev <ganchevdimitarg@gmail.com>
2023-10-31 11:38:46 +0200

    swagger

--- catalog/src/main/java/com/concordeu/catalog/service/product/ProductService.java
+++ catalog/src/main/java/com/concordeu/catalog/service/product/ProductService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ProductService {
-
     ProductDTO createProduct(ProductDTO productResponseDto, String categoryName);
     Page<ProductDTO> getProductsByPage(int page, int size);
     Page<ProductDTO> getProductsByCategoryByPage(int page, int size, String categoryName);
@@ -15,6 +14,5 @@ public interface ProductService {
     ProductDTO getProductById(UUID id);
     void updateProduct(ProductDTO productResponseDto, String productName);
     void deleteProduct(String productName);
-
     List<ProductDTO> getProductsById(List<UUID> items);
 }
