3a111d14385f9ccf70c55fcef96cdf6fd3b20175
ganchevdimitarg <ganchevdimitarg@gmail.com>
2026-06-22 23:53:08 +0300

    build(catalog, client): update Java to 25, enhance Redis caching, and add Lombok dependency

--- catalog/src/main/java/com/concordeu/catalog/config/RedisCacheConfig.java
+++ catalog/src/main/java/com/concordeu/catalog/config/RedisCacheConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -22,7 +23,11 @@ public class RedisCacheConfig {
                 .prefixCacheNameWith("catalog:")
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
                         .fromSerializer(new GenericJackson2JsonRedisSerializer()));
-        return RedisCacheManager.builder(connectionFactory)
+
+        RedisCacheWriter cacheWriter = RedisCacheWriter.create(connectionFactory,
+                writer -> writer.immediateWrites());
+
+        return RedisCacheManager.builder(cacheWriter)
                 .cacheDefaults(config)
                 .build();
     }
